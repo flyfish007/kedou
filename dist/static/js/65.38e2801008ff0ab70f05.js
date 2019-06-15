191e691522507cd4038b0c364fc3f8ef364c6777
guofangfang <guofangfang@enmonster.com>
2019-06-15 12:24:55 +0800

    build

---
+++ dist/static/js/65.38e2801008ff0ab70f05.js
@@ -0,0 +1,2 @@
+webpackJsonp([65],{zR6y:function(e,t,r){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var a={name:"addCartView",data:function(){return{proDetail:null}},computed:{},mounted:function(){this.$route.query.proDetail&&(this.proDetail=JSON.parse(this.$route.query.proDetail),this.$refs.addCart.load(this.proDetail,!0))}},n={render:function(){var e=this.$createElement;return(this._self._c||e)("add-cart",{ref:"addCart"})},staticRenderFns:[]},i=r("C7Lr")(a,n,!1,null,null,null);t.default=i.exports}});
+//# sourceMappingURL=65.38e2801008ff0ab70f05.js.map
